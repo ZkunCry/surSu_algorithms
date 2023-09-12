5bee2c01d8664a8dc4cc0073b4142a1902627a0d
ZkunCry <aksentev.03@mail.ru>
2023-09-12 22:21:37 +0500

    finished work

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -11,14 +11,24 @@ void FillUp(int arr[], int size, int min, int max);
 void FillDown(int arr[], int size, int min, int max);
 void fillSawtoot(int arr[], int size, int min, int max, int interval);
 void fillSin(int arr[], int size, int min, int max, int interval);
-void printArr(int arr[], int size);
+
+void printArr(int arr[], int size, int interval);
 
 
-
-void printArr(int arr[], int size)
+void printInterval(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i]<<std::endl;
+	}
+	std::cout << std::endl;
+}
+void printArr(int arr[], int size,int interval)
 {
-	for (int i = 0; i < size; i++)
-		std::cout << arr[i] << std::endl;
+	int L = size / interval;
+	for (int i = 0; i < interval; i++)
+	{
+		printInterval(&arr[L * i], L);
+	}
+	
 }
 int randAB(int min, int max) 
 {
@@ -64,7 +74,6 @@ void fillSin(int arr[], int size, int min, int max, int interval) {
 			std::reverse(arr + (L * i), arr + (L * i) + L);
 }
 void fillQuaisie(int arr[], int size, int min, int max, int interval) {
-	//fillSawtoot(arr, size, min, max, interval);
 	int L = size / interval, j = 0;
 	for (int i = 0; i < interval; i++)
 	{
@@ -84,32 +93,32 @@ void fillQuaisie(int arr[], int size, int min, int max, int interval) {
 		std::swap(arr[ stepLeft], arr[stepLeft + 1]);
 	}
 }
-void fillRandStep(int arr[], int size, int min, int max, int interval) {
+inline void fillRandStep(int arr[], int size, int min, int max, int interval) {
 	int L = size / interval;
+
 	for (int i = 0; i < interval; i++) {
-		FillRand(&arr[L*i],L,min, min + (max / interval));
+		
+		FillRand(&arr[L*i],L,min,  (min + ((max / interval))));
 		min += max / interval;
 	}
 	int j = 0;
 	if ((L * interval) < size) {
-		double h = (((double(max - min))) / L);
+		double h = abs((((double(max - min))) / L));
 		for (int i = L * interval; i < size; i++,j++)
 			arr[i] = h * j + min;
 	}
+
 }
 int main()
 {
-	constexpr int MAX_LENGTH = 1000000;
+	constexpr long MAX_LENGTH = 400000000;
 	int  * arr = new int[MAX_LENGTH];
-	/*for (int N = MAX_LENGTH / 10; N <= MAX_LENGTH; N += MAX_LENGTH / 10)
+	for (int N = MAX_LENGTH / 200; N <= MAX_LENGTH / 20; N += MAX_LENGTH / 200)
 	{
 		int t1 = GetTickCount();
-		FillUp(arr, MAX_LENGTH, 1, 20);
-		std::cout << GetTickCount() - t1<<std::endl;
-	}*/
-	fillRandStep(arr, 10, 1,10 , 2);
-	printArr(arr, 10);
-
+		fillQuaisie(arr, N, 2, N,10);
+		std::cout << (GetTickCount() - t1)<<std::endl;
+	}
 
 	return 0;
 
