bd0de59d87b1e0a86d85e15738ba15916d1252d1
ZkunCry <aksentev.03@mail.ru>
2023-09-14 22:39:25 +0500

    finished work

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -16,18 +16,15 @@ void printArr(int arr[], int size, int interval);
 
 
 void printInterval(int arr[], int size) {
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) 
 		std::cout << arr[i]<<std::endl;
-	}
 	std::cout << std::endl;
 }
 void printArr(int arr[], int size,int interval)
 {
 	int L = size / interval;
 	for (int i = 0; i < interval; i++)
-	{
 		printInterval(&arr[L * i], L);
-	}
 	
 }
 int randAB(int min, int max) 
@@ -54,38 +51,33 @@ void FillDown(int arr[], int size, int min, int max)
 void fillSawtoot(int arr[], int size, int min, int max,int interval)
 {
 	int L = size / interval,j=0;
-
+	double h = (((double(max - min))) / (L));
 	for (int i = 0; i < interval; i++) 
 		FillUp(&arr[L * i], L, min, max);
 
-	
 	if ((L * interval) < size) 
-	{
-		double h =  (((double(max - min))) / (L));
 		for (int i = L * interval; i < size; i++,j++)
 			arr[i] = h * j + min;
-	}
 }
 void fillSin(int arr[], int size, int min, int max, int interval) {
-	fillSawtoot(arr, size, min, max, interval);
-	int L = size / interval;
+	fillSawtoot(arr, size, min, max, interval*2);
+	int L = size / (interval+interval);
 	for (int i = 0; i <= interval; i++)
 		if (i % 2 == 0)
 			std::reverse(arr + (L * i), arr + (L * i) + L);
 }
 void fillQuaisie(int arr[], int size, int min, int max, int interval) {
 	int L = size / interval, j = 0;
+	double h = (((double(max - min))) / (L));
 	for (int i = 0; i < interval; i++)
 	{
 		if(i >0)
 			min = arr[(L * i)-1]+1;
 		FillUp(&arr[L * i], L, min, max);
 	}
-	if ((L * interval) < size) {
-		double h = (((double(max - min))) / (L));
+	if ((L * interval) < size) 
 		for (int i = L * interval; i < size; i++, j++)
 			arr[i] = h * j + min;
-	}
 	 j = 1;
 	for (int i = 0; i < interval; i++,j++)
 	{
@@ -111,14 +103,19 @@ inline void fillRandStep(int arr[], int size, int min, int max, int interval) {
 }
 int main()
 {
+	system("chcp 1251 > null");
 	constexpr long MAX_LENGTH = 400000000;
 	int  * arr = new int[MAX_LENGTH];
-	for (int N = MAX_LENGTH / 200; N <= MAX_LENGTH / 20; N += MAX_LENGTH / 200)
+	int j = 1;
+
+	for (int N = MAX_LENGTH / 200; N <= MAX_LENGTH / 2; N += MAX_LENGTH / 200)
 	{
-		int t1 = GetTickCount();
-		fillQuaisie(arr, N, 2, N,10);
-		std::cout << (GetTickCount() - t1)<<std::endl;
+		auto t1 = GetTickCount();
+		fillSin(arr, N, 1, N, 3);
+		std::cout << "(" << N/10000 << ','<< (GetTickCount() - t1) <<')'  << std::endl;
+		j++;
 	}
+	
 
 	return 0;
 
