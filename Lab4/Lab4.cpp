574727e53cca7ee26e751b5b0350968a83bb9d8e
ZkunCry <aksentev.03@mail.ru>
2023-10-09 18:55:28 +0500

    fixed some bugs

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,14 +1,30 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <functional>
 #include <fstream>
 #include <Windows.h>
 #include <string>
 #include <sstream>
+#include <iterator>
 using std::endl;
 using std::cout;
 using std::string;
 
+
+int randAB(int min, int max)
+{
+	return (((rand()) << 15) | rand())
+		% (max - min) + min;
+}
+double db_randAB(double min, double max) {
+	return min + fmod(static_cast<double>(rand()), max - min);
+}
+
+void FillRand(string src, int size, char min, char max)
+{
+	for (int i = 0; i < size; i++)
+		src[i] = (char)db_randAB(min, max);
+}
 typedef struct methodHorner {
 public:
 	int Q = 256; //Необходимо для избежания больших чисел хэша
@@ -28,10 +44,12 @@ long int get_hash(const string& src) {
 	return result;
 }
 int search_pattern(const string& src, const string& pattern) {
+
 	int multiplier = 1,count = 0;
 	for (int i = 1; i < pattern.length(); i++) {
 		multiplier = (multiplier * variables.B) % variables.Q;
 	}
+
 	auto pattern_hash = get_hash(pattern);
 	auto src_hash = get_hash(src.substr(0,pattern.length()));
 	
@@ -43,15 +61,17 @@ int search_pattern(const string& src, const string& pattern) {
 				count++;
 		}
 		if (i < (src.length() - pattern.length())) {
-			src_hash = ((src_hash - char(src[i]) * multiplier) * variables.B + char(src[i + pattern.length()])) % variables.Q;
+			src_hash = ((src_hash - char(src[i]) * multiplier) * variables.B 
+				+ char(src[i + pattern.length()])) % variables.Q;
 			if (src_hash < 0)
 				src_hash += variables.Q;
 		}
 	}
 	
-	return count >0 ? count : -1;
+	return count > 0 ? count : -1;
 
 }
+
 
 int main()
 {
@@ -60,23 +80,32 @@ int main()
 	ss << in.rdbuf();
 	
 	string text;
+
 	text = ss.str();
 	in.close();
 
-	const int N_MAX = text.size();
-	const int Step = N_MAX / 5;
-	auto tempN = 0;
-	int d = 10;
-	for (int N = Step; N <= N_MAX; N += Step)
-	{
-		auto mainText = text.substr(0, N);
-		auto patternText = mainText.substr(tempN, d);
-		auto time = GetTickCount();
-		auto result = search_pattern(text, patternText);
-		cout << GetTickCount() - time <<" "<<mainText.size() << endl;
-		cout << "Result: " << result << endl;
-		d *= 10;
-		tempN = N;
+	const int N_MAX = text.length();
+	const int countIter = 5;
+	const int Step = N_MAX / countIter;
+
+	int M = 10;
+		for (int N = Step; N <= N_MAX; N += Step)
+		{
+			cout << "Size of search string: " << M << endl << "Size of main text:" << N_MAX << endl;
+			for (int i = 0; i < countIter; i++)
+			{
+				auto mainText = text.substr(0, N);
+				int id = randAB(0, N - M - 1);
+				auto patternText = mainText.substr(id, M);
+				auto time = GetTickCount64();
+				auto result = search_pattern(text, patternText);
+				cout << GetTickCount64() - time << " " << mainText.size() << endl;
+				cout << "Result: " << result << endl;
+			}
+			cout << endl;
+			cout << endl;
+			M *= 10;
+		}
+		
 	}
 
-}
